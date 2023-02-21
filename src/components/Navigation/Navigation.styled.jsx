59755f95da62c6d27de092a7210228030c67b5a6
SerhiySBV <serhiybelyavskii@gmail.com>
2023-02-21 06:52:38 +0200

    Update Navigation.styled.jsx

--- src/components/Navigation/Navigation.styled.jsx
+++ src/components/Navigation/Navigation.styled.jsx
@@ -9,10 +9,11 @@ export const Header = styled.div`
   justify-content: flex-start;
   margin-bottom: 20px;
 `;
+
 const StyledLink = styled(NavLink)`
   border: 1px solid;
   padding: 5px;
-  border-radius: 5px;
+  border-radius: 6px;
   color: darkslategray;
 `;
 export default StyledLink;
